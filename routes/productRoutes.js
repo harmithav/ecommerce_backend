4f2a5784525a0130d685e781021ed3f5e29d68cb
Harmitha <harmithavenkatesan5@gmail.com>
2026-05-08 22:11:13 +0530

    updated account button style

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -85,7 +85,7 @@ router.get("/", (req, res) => {
       name: "Bluetooth Speaker",
       price: 2499,
       category: "Electronics",
-      image: "https://www.imastudent.com/jbl-partybox-310-portable-bluetooth-speaker-with-party-lights?srsltid=AfmBOoqYssEaexWfQsFFtx3Aj8z1mjiTqwbHOMb_rSLjQ-7XSg9ewKOh?w=300"
+      image: "https://www.imastudent.com/jbl-partybox-310-portable-bluetooth-speaker-with-party-lights?srsltid=AfmBOop4zw_9mAOF6VIiYdno0lts65_Fw6SmDybGK4AhDnkMt_kQKudb?w=300"
     },
 
     // ================= KITCHEN =================
@@ -101,7 +101,7 @@ router.get("/", (req, res) => {
       name: "Cookware Set",
       price: 4599,
       category: "Kitchen",
-      image: "https://www.indiamart.com/proddetail/nonstick-cookware-set-2851653793333.html?srsltid=AfmBOoq5vAhaftAnuYbUQfJNt1LZD5rrrytQbpYWsijGS4BD2U-INSsu?w=300"
+      image: "https://nestasia.in/products/triply-cookware-set-with-kadai-frying-pan-and-sauce-pan?srsltid=AfmBOooW8uzAZIHtus5dq67-REKGh9S1129SiT__nWju3hcejg8q9Mwq?w=300"
     },
 
     {
